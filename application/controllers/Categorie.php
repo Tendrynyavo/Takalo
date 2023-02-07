a38a37a3bfd37b0d3ee3b1211eb7c4bd5c113ee0
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-02-07 19:57:16 +0200

    dynamiser categorisation

--- application/controllers/Categorie.php
+++ application/controllers/Categorie.php
@@ -13,6 +13,14 @@ class Categorie extends CI_Controller {
 		$data = [];
 		$data['categories'] = $this->categorie_model->get_categorie();
 		$data['objets'] = ($this->input->get('categorie') == null) ? $this->objet_model->get_by_idcategorie(1) : $this->objet_model->get_by_idcategorie($this->input->get('categorie'));
+		
 		$this->load->view('category', $data);
+	}
+
+	public function update() {
+		$data = [];
+		$data['objet'] = $this->object_model->get_by_id($this->input->get('id'))[0];
+
+		$this->load->view('categorisation', $data);
 	}
 }
