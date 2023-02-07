105378b2e9e66b5974346ab6c7be94652b76bf59
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2023-02-08 00:33:12 +0300

    deconnecte commit

--- application/controllers/Objet.php
+++ application/controllers/Objet.php
@@ -6,13 +6,23 @@ class Objet extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('objet_model');
-		$this->load->library('session');
 	}
 
 	public function index() {
         $data = array();
-        $data['objets'] = $this->objet_model->;
+        $data['user'] = $this->session->user;
+        $data['objets'] = $this->objet_model->get_not_owned($this->session->user['id']);
         $data['content'] = 'liste_objet';
-		$this->load->view('login' $data);
+		$this->load->view('template', $data);
 	}
+	
+    public function gestion() {
+        $data = array();
+        $data['user'] = $this->session->user;
+        $data['objets'] = $this->objet_model->get_by_id($this->session->user['id']);
+        $data['content'] = 'gestion_objet';
+		$this->load->view('template', $data);
+	}
+
+
 }
--- application/views/gestion_objet.php
+++ application/views/gestion_objet.php
@@ -47,24 +47,24 @@
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 
-                <?php for ($i=0; $i < 3; $i++) { ?>
+                <?php foreach ($objets as $objet) { ?>
                 
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="<?= base_url('assets/img/macbook.png') ?>" alt="..." />
+                            <img class="card-img-top" src="<?php echo base_url('assets/img/macbook.png'); ?>" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h4 class="fw-bolder"><strong>MacBook</strong></h4>
+                                    <h4 class="fw-bolder"><strong><?php echo $objet['nom']; ?></strong></h4>
                                     <!-- Product price-->
-                                    $2400
+                                    <?php echo $objet['prix']; ?>
                                     <br>
                                     <br>
                                 </div>
                                 <!-- Product description-->
-                                <p><strong>Description: </strong>M2 Pro tena hiaka farany an'ny Apple</p>
+                                <p><strong>Description: </strong><?php echo $objet['descr']; ?></p>
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
