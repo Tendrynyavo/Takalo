e0ab5f998addf95752638bfd9274aa1e218b062e
Pakito033 <119033755+Pakito033@users.noreply.github.com>
2023-02-07 12:00:09 +0300

    C:\Users\Tim\Documents\sql\schema.sql

---
+++ schema.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE takalo;
+USE takalo;
+
+CREATE TABLE bool(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  value VARCHAR(10)
+);
+
+CREATE TABLE user(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
+    mdp VARCHAR(30) NOT NULL,
+    estAdmin int NOT NULL REFERENCES bool(id)
+);
+
+CREATE TABLE objet(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idUser int REFERENCES user(id),
+    descr TEXT NOT NULL,
+    prix DOUBLE PRECISION
+);
+
+CREATE TABLE photo(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    photo VARCHAR(100) NOT NULL,
+    idObjet int NOT NULL REFERENCES objet(id)
+);
+
+CREATE TABLE echange(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idObjet1 int NOT NULL REFERENCES objet(id),
+    idObjet2 int NOT NULL REFERENCES objet(id),
+    submitted int NOT NULL REFERENCES bool(id)
+);
+
+INSERT INTO bool (value) VALUES
+('true'),
+('false');
+
+INSERT INTO user(email, mdp, estAdmin) VALUES
+('timmypablojamon@gmail.com', '1928', 1),
+('tendrynyavo@gmail.com', '2070', 1),
+('mpiahysoa@gmail.com', '2036', 1),
+('bob@gmail.com', '1234', 2);
+
