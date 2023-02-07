c4a47b47408e7bf122753b592b764c61d4800318
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-02-07 18:06:03 +0200

    dynamiser category

--- application/controllers/Categorie.php
+++ application/controllers/Categorie.php
@@ -3,9 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Categorie extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('categorie_model');
+		$this->load->model('objet_model');
+	}
+
 	public function index() {
 		$data = [];
-		$data['categories'] = $this->categorie->
-		$this->load->view('category');
+		$data['categories'] = $this->categorie_model->get_categorie();
+		$data['objets'] = ($this->input->get('categorie') == null) ? $this->objet_model->get_by_idcategorie(1) : $this->objet_model->get_by_idcategorie($this->input->get('categorie'));
+		$this->load->view('category', $data);
 	}
 }
