feac2fceaf9d1d083fc5423aa66c268586be9587
Pakito033 <timmypablojamon@gmail.com>
2023-02-07 18:47:32 -0500

    donnees de test

--- bdd/data.sql
+++ bdd/data.sql
@@ -11,4 +11,13 @@ INSERT INTO user(nom, prenom, email, mdp, etat) VALUES
 ('Bob', 'Stone', 'bob@gmail.com', '1234', 0);
 
 INSERT INTO objet(idUser, nom, idCategorie, descr, prix) VALUES
-(1, 'Le Docteur Amoureux', 2, 'Un petit Molière', 2000.00);
+(1, 'Le Docteur Amoureux', 2, 'Un petit Molière', 2000.00),
+(4, 'Nike', 4, 'Un petit Molière', 200.00),
+(4, 'Devil May Cry 5', 4, 'Bon état', 25000.00),
+(4, 'Converses', 4, 'Noires', 150.00),
+(3, 'T-shirt Spider-Man', 3, 'Il pu', 2000.00),
+(3, 'Huawei nova 2', 4, 'Bon état', 325000.00),
+(4, 'Souris Gamer', 4, 'Noire et verte, sans néons', 12000.00),
+(2, 'Shining', 4, 'Un livre de Stephen King', 2000.00),
+(4, 'Doctor Sleep', 4, 'Shining, la suite', 2300.00),
+(4, 'GTA V', 4, 'PS3', 15000.00);
