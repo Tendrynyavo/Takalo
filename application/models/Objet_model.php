dea9b76ed693fdd3aa5cfd482b9aca848ac6ecea
Pakito033 <timmypablojamon@gmail.com>
2023-02-07 17:35:34 -0500

    Fonction modifier objet

--- application/models/Objet_model.php
+++ application/models/Objet_model.php
@@ -18,12 +18,20 @@ class Objet_model extends CI_Model {
         return convert_to_array($query);
     }
     
-/// Fonction pour modifier un objet
-    public function modif_objet($id_categorie = '', $id_objet='') {
+/// Fonction pour modifier la catégorie d'un objet
+    public function modif_categorie($id_categorie = '', $id_objet='') {
         $sql = 'UPDATE objet SET idCategorie = %d WHERE id = %d';
         $sql = sprintf($sql, $this->db->escape($id_categorie), $this->db->escape($id_objet));
         $query = $this->db->query($sql);
     }
+
+/// Fonction pour modifier un objet
+    public function modif_objet($id_categorie = '', $descr='', $prix='', $id_objet='') {
+        $sql = 'UPDATE objet SET idCategorie = %d, descr = %s,  prix = %d WHERE id = %d';
+        $sql = sprintf($sql, $this->db->escape($id_categorie), $this->db->escape($descr), $this->db->escape($prix), $this->db->escape($id_objet));
+        $query = $this->db->query($sql);
+    }    
+
 
 /// Fonction ajouter un objet au site
     public function ajouter_objet($id_user='', $id_categorie='', $descr = '', $prix='') {
