3293037f2e678231d306d71d78b136c65d6a37d4
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-02-07 13:46:17 +0200

    login controlleur

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,1 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Login extends CI_Controller {
+
+	public funtion __construct() {
+		parent::__construct();
+		$this->load->model('user_model');
+	}
+
+	public function index() {
+		$this->load->view('login');
+	}
+
+	public function login() {
+		$user = $this->user_model->check_user($this->input->post('email'), $this->input->post('password'));
+		if (count($user) > 0) {
+			$this->session->set_userdata('user', $user[0]);
+			echo $this->session->user['email'];
+		} else {
+			redirect(base_url('login'));
+		}
+	}
+}
