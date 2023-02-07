01290e510e60fbc7553db69edb27cbe7e200a377
Mpiahy <mpiahyandriam@gmail.com>
2023-02-07 13:05:29 +0300

    user_login & sign up commit

--- application/views/login.php
+++ application/views/login.php
@@ -6,31 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css')?>">
     <link rel="icon" href="<?= base_url('assets/img/responsabilite-sociale.png')?>">
-    <title>Login Admin Takalo Takalo</title>
+    <title>Login Takalo Takalo</title>
 </head>
 <body>
     <nav id="mainNav" class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3">
         <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span>Takalo Takalo</span></a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div id="navcol-1" class="collapse navbar-collapse">
                 <ul class="navbar-nav mx-auto">
-                    <li class="nav-item"><a class="nav-link active" href="login.html">Log in</a></li>
-                    <li class="nav-item"><a class="nav-link" href="signup.html">Sign up</a></li>
-                </ul><a class="btn btn-primary shadow" role="button" href="signup.html">Sign up</a>
+                    <li class="nav-item"><a class="nav-link active" href="#">Log Up</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Sign Up</a></li>
+                </ul>
             </div>
         </div>
     </nav>
     <section class="py-4 py-md-5 my-5">
         <div class="container py-md-5">
             <div class="row">
-                <div class="col-md-6 text-center"><img class="img-fluid w-50" src="<?= base_url('assets/img/authorization.png')?>" /></div>
+                <div class="col-md-6 text-center"><img class="img-fluid w-50" src="<?= base_url('assets/img/echanger.png')?>" /></div>
                 <div class="col-md-5 col-xl-4 text-center text-md-start">
-                    <h2 class="display-6 fw-bold mb-5"><span class="underline pb-1"><strong>Admin Login</strong><br /></span></h2>
-                    <form method="post">
+                    <h2 class="display-6 fw-bold mb-5"><span class="underline pb-1"><strong>User Login</strong><br /></span></h2>
+                    <form method="post" action="#">
                         <div class="mb-3"><input class="shadow form-control" type="email" name="email" placeholder="Email" /></div>
                         <div class="mb-3"><input class="shadow form-control" type="password" name="password" placeholder="Password" /></div>
-                        <div class="mb-5"><button class="btn btn-primary shadow" type="submit">Log in</button></div>
+                        <div class="mb-5"><button class="btn btn-primary shadow" type="submit">Se Connecter</button></div>
                         <p class="text-muted"></p>
                     </form>
+
+                    <div class="mb-2">
+                            <p>Vous n'avez pas de compte? 
+                                <a class="btn btn-secondary shadow" role="button" href="#">S'inscrire</a>
+                            </p>
+                    </div>
                 </div>
             </div>
         </div>
