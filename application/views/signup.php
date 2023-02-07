a80e005839a20919f63d8254c30136736661c9f7
Mpiahy <mpiahyandriam@gmail.com>
2023-02-07 14:53:36 +0300

    category_page commit v2

--- application/views/signup.php
+++ application/views/signup.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css')?>">
     <link rel="icon" href="<?= base_url('assets/img/responsabilite-sociale.png')?>">
-    <title>Login Takalo Takalo</title>
+    <title>Sign Up Takalo Takalo</title>
 </head>
 <body>
     <nav id="mainNav" class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3">
