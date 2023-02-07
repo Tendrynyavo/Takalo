8cd2d7438136b023573eb7b2b68e125a330198c9
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-02-07 15:07:55 +0200

    session commit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,6 +6,7 @@ class Login extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('user_model');
+		$this->load->library('session');
 	}
 
 	public function index() {
