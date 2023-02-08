ad5ee2c015129aac074a242d027016c7a4332f28
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2023-02-08 16:15:43 +0300

    liste objet

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'takalo',
+	'hostname' => 'sql302.epizy.com',
+	'username' => 'epiz_33550188',
+	'password' => 'lxxGy02ZfWObiz',
+	'database' => 'epiz_33550188_takalo',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
