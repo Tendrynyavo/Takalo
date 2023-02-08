862f4a83acfe7a6615e9ed756e7890f8e63a0b51
Pakito033 <timmypablojamon@gmail.com>
2023-02-08 07:26:55 -0500

    objet namboarina

--- application/models/Objet_model.php
+++ application/models/Objet_model.php
@@ -12,7 +12,7 @@ class Objet_model extends CI_Model {
 
 /// Fonction pour obtenir un objet par son id
     public function get_by_id($id = 1) {
-        $sql='SELECT * FROM objet WHERE id = %s';
+        $sql='SELECT * FROM objet o JOIN user u ON o.idUser=u.id WHERE id = %s';
         $sql = sprintf($sql, $this->db->escape($id));
         $query = $this->db->query($sql); 
         return $query->row();
@@ -51,14 +51,14 @@ class Objet_model extends CI_Model {
 
 /// Fonction pour modifier un objet
     public function modif_objet($nom='', $descr='', $prix='', $id_objet='') {
-        $sql = 'UPDATE objet SET nom=%s, descr = %s,  prix = %d WHERE id = %d';
+        $sql = 'UPDATE objet SET nom=%s, descr = %s,  prix = %s WHERE id = %s';
         $sql = sprintf($sql, $this->db->escape($nom), $this->db->escape($descr), $this->db->escape($prix), $this->db->escape($id_objet));
         $query = $this->db->query($sql);
     }    
 
 /// Fonction ajouter un objet au site
     public function ajouter_objet($id_user='',$nom='', $id_categorie='', $descr = '', $prix='') {
-        $sql = 'INSERT INTO objet (idUser, nom, idCategorie, descr, prix) VALUES (%s, %s, %s, %s, %d)';
+        $sql = 'INSERT INTO objet (idUser, nom, idCategorie, descr, prix) VALUES (%s, %s, %s, %s, %s)';
         $sql = sprintf($sql, $this->db->escape($id_user), $this->db->escape($nom), $this->db->escape($id_categorie), $this->db->escape($descr), $this->db->escape($prix));
         $query = $this->db->query($sql);
     }
