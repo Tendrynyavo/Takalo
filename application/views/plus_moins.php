32a78ca02c555faa28d7ad4c619e0ac161c96c19
Mpiahy <mpiahyandriam@gmail.com>
2023-02-08 16:36:06 +0300

    filtre

--- application/views/plus_moins.php
+++ application/views/plus_moins.php
@@ -1,7 +1,7 @@
 <!-- PAGE D'ACCUEIL -->
 <!-- PAGE D'ACCUEIL -->
 <!-- PAGE D'ACCUEIL -->
-
+<?php $objets=;?>
 <!-- Header-->
 <header class="bg-dark pt-5 pb-2">
     <div class="container px-1 px-lg-5 my-5">
@@ -16,32 +16,32 @@
 <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <?php //foreach ($objets as $objet) { ?>
+            <?php foreach ($objets as $objet) { ?>
 
                 <div class="col mb-5">
                     <div class="card h-100">
                         <!-- Product image-->
-                        <img class="card-img-top" src="<?php //echo base_url($objet['photo'][0]['photo']); ?>" alt="..." />
+                        <img class="card-img-top" src="<?php echo base_url($objet['photo'][0]['photo']); ?>" alt="..." />
                         <!-- Product details-->
                         <div class="card-body p-4">
                             <div class="text-center">
                                 <!-- Product name-->
                                 <h5 class="fw-bolder">
-                                    <?php //echo $objet['nom']; ?>
+                                    <?php echo $objet['nom']; ?>
                                 </h5>
                                 <!-- Product price-->
-                                <?php //echo $objet['prix']; ?>
+                                <?php echo $objet['prix']; ?>
                             </div>
                         </div>
                         <!-- Product actions-->
                         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                             <div class="text-center"><a class="btn btn-outline-dark mt-auto"
-                                    href="<?php //echo base_url('index.php/echange?id=' . $objet['id']); ?>">Proposer d'echanger</a></div>
+                                    href="<?php echo base_url('index.php/echange?id=' . $objet['id']); ?>">Proposer d'echanger</a></div>
                         </div>
                     </div>
                 </div>
 
-            <?php //} ?>
+            <?php } ?>
 
         </div>
     </div>
